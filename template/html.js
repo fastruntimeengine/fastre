024c31cb7efea9873a016a1159f44ecc881428ef
mvishok <vishokmanikantan@gmail.com>
2024-11-30 23:44:31 +0530

    fix: Improve body attribute handling in renderHTML function to avoid null values

--- template/html.js
+++ template/html.js
@@ -101,16 +101,9 @@ export default async function renderHTML($){
             }
             
             try{
-                rbody = $(tag).attr('body') ? JSON.parse($(tag).attr('body')) : null;
-                if (rbody == null){
-                    rbody = strRender($(tag).attr('body'));
-                }
+                rbody = $(tag).attr('body') ? JSON.parse($(tag).attr('body')) : "";
             } catch {
-                log(`[OUT] [${method}] ${url} failed`, 'error');
-                log("Invalid JSON", 'error');
-                $(tag).replaceWith("<span></span>");
-                tags = $(tagsString)
-                continue;
+                rbody = strRender($(tag).attr('body'));
             }
 
             const id = $(tag).attr('id');
