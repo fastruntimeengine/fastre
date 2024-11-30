bbecb6ae311e4387bdbfd58ecd572f2b97b00663
mvishok <vishokmanikantan@gmail.com>
2024-11-30 19:53:24 +0530

    refactor: Update post function to be synchronous

--- server/param.js
+++ server/param.js
@@ -12,29 +12,38 @@ function get(url){
     return true;
 }
 
-function post(req){
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', () => {
-        if (body) {
-            try {
-                const parsed = JSON.parse(body);
-                for (const key of Object.keys(parsed)) {
-                    data[key] = parsed[key];
+function post(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            if (body) {
+                try {
+                    const parsed = JSON.parse(body);
+                    for (const key of Object.keys(parsed)) {
+                        data[key] = parsed[key];
+                    }
+                    resolve(true);
+                } catch (err) {
+                    log('Invalid JSON', 'error');
+                    reject(err);
                 }
-                return true;
-            } catch (err) {
-                log('Invalid JSON', 'error');
+            } else {
+                resolve(true);
             }
-        }
+        });
+        req.on('error', err => {
+            log('Request error', 'error');
+            reject(err);
+        });
     });
 }
 
-export default function getParams(req){
+export default async function getParams(req) {
     const method = req.method;
     const url = req.url;
-    const params = method === 'GET' ? get(url) : post(req);
+    const params = method === 'GET' ? get(url) : await post(req);
     return params;
 }
